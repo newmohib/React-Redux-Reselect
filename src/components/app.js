f550d86fa72f966538ed023fd833255adae1e93e
unknown <newmohib@gmail.com>
2019-05-21 18:12:09 +0600

    first commit

---
+++ src/components/app.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Component } from 'react';
+
+import Posts from 'components/posts_list';
+import SelectedPostsList from 'components/selected_posts_list';
+
+export default () => {
+  return (
+    <div>
+      <h4>Selected Posts</h4>
+      <SelectedPostsList />
+      <hr />
+      <br />
+      <h4>All Posts</h4>
+      <Posts />
+    </div>
+  );
+}
